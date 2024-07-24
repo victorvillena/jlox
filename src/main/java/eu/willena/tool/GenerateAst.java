5111dc2071b8703295f5ab46d6d260a6770405be
Víctor Villena <knbosh@gmail.com>
2024-07-24 02:41:59 +0200

    Add GenerateAst tool and the generated Expr class

---
+++ src/main/java/eu/willena/tool/GenerateAst.java
@@ -0,0 +1,75 @@
+package eu.willena.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.err.println("Current dir: " + System.getProperty("user.dir"));
+            System.exit(64);
+        }
+        var outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(
+            String outputDir,
+            String baseName,
+            List<String> types
+    ) throws IOException {
+        var path = outputDir + "/" + baseName + ".java";
+        var writer = new PrintWriter(path, "UTF-8");
+
+        writer.println("package eu.willena.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        for (var type : types) {
+            var className = type.split(":")[0].trim();
+            var fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineType(
+            PrintWriter writer,
+            String baseName,
+            String className,
+            String fieldList
+    ) {
+        // Class declaration
+        writer.println("    static class " + className + " extends " + baseName + " {");
+
+        // Constructor
+        writer.println("        " + className + "(" + fieldList + ") { ");
+        var fields = fieldList.split(", ");
+        for (var field : fields) {
+            var name = field.split(" ")[1];
+            writer.println("            this." + name + " = " + name + ";");
+        }
+        writer.println("        }");
+
+        // Fields
+        writer.println();
+        for (var field : fields) {
+            writer.println("        final " + field + ";");
+        }
+
+        writer.println("    }");
+    }
+}
