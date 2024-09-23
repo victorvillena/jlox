6c2d2d9b9ac2083a7a51341929dd3339463c3767
Víctor Villena <knbosh@gmail.com>
2024-09-23 20:43:07 +0200

    Implement function declaration

--- src/main/java/eu/willena/lox/Parser.java
+++ src/main/java/eu/willena/lox/Parser.java
@@ -28,8 +28,13 @@ class Parser {
     /*
     program        -> declaration* EOF ;
 
-    declaration    -> varDecl
+    declaration    -> funDecl
+                    | varDecl
                     | statement ;
+
+    funDecl        -> "fun" function ;
+    function       -> IDENTIFIER "(" parameters? ")" block ;
+    parameters     -> IDENTIFIER ( "," IDENTIFIER )* ;
 
     varDecl        -> "var" IDENTIFIER ( "=" expression )? ";" ;
 
@@ -70,6 +75,7 @@ class Parser {
 
     private Stmt declaration() {
         try {
+            if (match(FUN)) return function("function");
             if (match(VAR)) return varDeclaration();
 
             return statement();
@@ -177,6 +183,27 @@ class Parser {
         Expr value = expression();
         consume(SEMICOLON, "Expect ';' after value.");
         return new Stmt.Expression(value);
+    }
+
+    private Stmt function(String kind) {
+        var name = consume(IDENTIFIER, "Expect " + kind + " name.");
+        consume(LEFT_PAREN, "Expect '(' after " + kind + " name.");
+        List<Token> parameters = new ArrayList<>();
+        if (!check(RIGHT_PAREN)) {
+            do {
+                if (parameters.size() >= 255) {
+                    error(peek(), "Can't have more than 255 parameters.");
+                }
+
+                parameters.add(consume(IDENTIFIER, "Expect parameter name."));
+            } while (match(COMMA));
+        }
+        consume(RIGHT_PAREN, "Expect ')' after parameters.");
+
+        consume(LEFT_BRACE, "Expect '{' before " + kind + " body.");
+        var body = block();
+
+        return new Stmt.Function(name, parameters, body);
     }
 
     private List<Stmt> block() {
