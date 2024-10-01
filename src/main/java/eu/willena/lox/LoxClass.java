1f8ca1ad4d44e0b892ba38b31d1e6e34a78dd21c
Víctor Villena <knbosh@gmail.com>
2024-10-01 18:08:05 +0200

    Enable 'init' as the constructor method for classes

--- src/main/java/eu/willena/lox/LoxClass.java
+++ src/main/java/eu/willena/lox/LoxClass.java
@@ -27,12 +27,21 @@ class LoxClass implements LoxCallable {
 
     @Override
     public int arity() {
-        return 0;
+        var initializer = findMethod("init");
+        if (initializer == null) {
+            return 0;
+        }
+        return initializer.arity();
     }
 
     @Override
     public Object call(Interpreter interpreter, List<Object> arguments) {
         LoxInstance instance = new LoxInstance(this);
+        var initializer = findMethod("init");
+        if (initializer != null) {
+            initializer.bind(instance).call(interpreter, arguments);
+        }
+
         return instance;
     }
 }
