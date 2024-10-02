6817d5e7958166e5122492d31391430b09b94860
Víctor Villena <knbosh@gmail.com>
2024-10-02 14:52:14 +0200

    Fix infinite loop in parser synchronizer
    My own mistake while working through the book. The improperly written
    synchronizer caused an infinite loop while trying to recover from badly
    written code, when the end of file was reached.

--- src/main/java/eu/willena/lox/Parser.java
+++ src/main/java/eu/willena/lox/Parser.java
@@ -469,20 +469,20 @@ class Parser {
 
         while (!isAtEnd()) {
             if (previous().type == SEMICOLON) return;
-        }
 
-        switch (peek().type) {
-            case CLASS:
-            case FUN:
-            case VAR:
-            case FOR:
-            case IF:
-            case WHILE:
-            case PRINT:
-            case RETURN:
-                return;
-        }
+            switch (peek().type) {
+                case CLASS:
+                case FUN:
+                case VAR:
+                case FOR:
+                case IF:
+                case WHILE:
+                case PRINT:
+                case RETURN:
+                    return;
+            }
 
-        advance();
+            advance();
+        }
     }
 }
